4fd34a75c87cddf04dd9481fd537bc829a3bc25d
unknown <ashik@example.com>
2024-09-07 22:26:23 +0530

    sepration of concern

---
+++ models/product-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const productSchema = mongoose.Schema({
+  images : String,
+  name: String ,
+  price : String,
+  discount : {
+    type : Number,
+    default : 0
+  },
+  bgcolor : String,
+  panelcolor : String,
+  textcolor : String,
+});
+module.exports =  mongoose.model('product' , productSchema);
